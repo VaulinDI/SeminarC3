673cc1d6297448573d4235d215e3701fb4b5b1e6
VaulinDI <vdi85@mail.ru>
2022-08-14 17:49:21 +0300

    arrayonezero task solved

---
+++ Example01_ArrayOneZero/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int n = 8;
+int[] array = new int[n];
+
+void FillArray(int[] array)
+{
+for (int i = 0; i < n; i++)
+{
+    array[i] = new Random().Next(0, 2);
+    Console.Write(array[i] + "   ");
+}
+}
+
+FillArray(array);
