d8e140c17abfa11e5652af7335cea5e7418c3fa5
VaulinDI <vdi85@mail.ru>
2022-08-14 20:46:09 +0300

    sumdigitsnoneven task solved

---
+++ Example07_SumDigitsEven/Program.cs
@@ -0,0 +1,31 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+Console.WriteLine("Введите длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+void FillArray(int[] array)
+{
+for (int i = 0; i < n; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    Console.Write(array[i] + "   ");
+}
+}
+
+FillArray(array);
+Console.WriteLine();
+
+int sum = 0;
+void SumNoneven()
+{
+for (int j = 0; j < n; j++)
+{
+    if (j % 2 != 0)
+    {
+        sum = sum + array[j];
+    }
+}
+Console.WriteLine($"Сумма чисел массива, стоящих на нечетной позиции равна: {sum}");
+}
+
+SumNoneven();
