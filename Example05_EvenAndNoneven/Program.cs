a95a3637bcbbfe0b224e1657d07828911e1f05e2
VaulinDI <vdi85@mail.ru>
2022-08-14 20:23:38 +0300

    even task solved

---
+++ Example05_EvenAndNoneven/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами
+// Показать количество нечетных\четных чисел
+Console.WriteLine("Введите длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+void FillArray(int[] array)
+{
+for (int i = 0; i < n; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(array[i] + "   ");
+}
+}
+
+FillArray(array);
+Console.WriteLine();
+
+int counteven = 0;
+int countnoneven = 0;
+
+void EvenDigits()
+{
+for (int j = 0; j < n; j++)
+{
+    if (array[j] % 2 == 0)
+    {
+        counteven++;
+    }
+    if (array[j] % 2 != 0)
+    {
+        countnoneven++;
+    }
+}
+Console.WriteLine($"Количество четных чисел в массиве равно {counteven}");
+Console.Write($"Количество нечетных чисел в массиве равно {countnoneven}");
+}
+
+EvenDigits();
