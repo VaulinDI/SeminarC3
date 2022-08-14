8a39cdbfd8222bc08e601cda8634156e08a3ed2f
VaulinDI <vdi85@mail.ru>
2022-08-14 20:36:05 +0300

    array123 task solved

---
+++ Example06_Array123/Program.cs
@@ -0,0 +1,30 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+int n = 123;
+int[] array = new int[n];
+
+void FillArray(int[] array)
+{
+for (int i = 0; i < n; i++)
+{
+    array[i] = new Random().Next(0, 1000);
+    Console.Write(array[i] + " ");
+}
+}
+
+FillArray(array);
+Console.WriteLine();
+
+int count = 0;
+void CountElements()
+{
+for (int j = 0; j < n; j++)
+{
+    if (array[j] >= 10 && array[j] <= 99)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество элементов из отрезка [10,99] равно {count}");
+}
+
+CountElements();
